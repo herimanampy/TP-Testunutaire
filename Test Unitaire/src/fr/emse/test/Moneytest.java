aff87dda91ce59cc78cb6d9a8f880d6a6220ffd3
herimanampy <herimanampy1411@gmail.com>
2024-04-03 07:48:40 +0200

    Premiere commit

---
+++ Test Unitaire/src/fr/emse/test/Moneytest.java
@@ -0,0 +1,30 @@
+package fr.emse.test;
+
+import static org.junit.Assert.*;
+
+import java.util.Objects;
+
+import org.junit.Test;
+
+public class Moneytest {
+
+	
+	@Test
+	public void testSimpleAdd() {
+	Money m12CHF = new Money(12, "CHF"); // création de données
+	Money m14CHF = new Money(14, "CHF");
+	Money expected = new Money(26, "CHF");
+	Money result = m12CHF.add(m14CHF); // exécution de la méthode testée
+	assertTrue(expected.equals(result)); // comparaison
+	}
+	@Test
+	public void testEquals() {
+	Money m12CHF= new Money(12, "CHF");
+	Money m14CHF= new Money(14, "CHF");
+	assertTrue(!m12CHF.equals(null)); 
+	assertEquals(m12CHF, m12CHF);
+	assertEquals(m12CHF, new Money(12, "CHF"));
+	assertTrue(!m12CHF.equals(m14CHF));
+	}
+}
+
